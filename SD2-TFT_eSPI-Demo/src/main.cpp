f4cc45bc9cbad7be23294b0abac5485fef767e88
huzongyao <huzongyao@tangbull.com>
2024-03-14 14:01:13 +0800

    init project

---
+++ SD2-TFT_eSPI-Demo/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+
+int LCD_BL_PWM = 5;
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  Serial.begin(115200);
+  tft.begin();
+  tft.fillScreen(TFT_BLACK);
+  pinMode(TFT_BL, OUTPUT);
+  analogWrite(TFT_BL, 1023 - (LCD_BL_PWM * 10));
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+  tft.drawCentreString("Hello World", 120, 120, 4);
+}
+
+void loop() {}
