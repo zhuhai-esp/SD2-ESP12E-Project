34958e536395f8fe960b0503badd5986a2e0ad96
some <some@gmail.com>
2024-11-02 21:46:48 +0800

    add lvgl9 time show

--- SD2-LVGL9-Demo/lib/lv_conf.h
+++ SD2-LVGL9-Demo/lib/lv_conf.h
@@ -484,13 +484,13 @@
 #define LV_FONT_MONTSERRAT_8  0
 #define LV_FONT_MONTSERRAT_10 0
 #define LV_FONT_MONTSERRAT_12 0
-#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_14 0
 #define LV_FONT_MONTSERRAT_16 0
 #define LV_FONT_MONTSERRAT_18 0
 #define LV_FONT_MONTSERRAT_20 0
 #define LV_FONT_MONTSERRAT_22 0
 #define LV_FONT_MONTSERRAT_24 0
-#define LV_FONT_MONTSERRAT_26 0
+#define LV_FONT_MONTSERRAT_26 1
 #define LV_FONT_MONTSERRAT_28 0
 #define LV_FONT_MONTSERRAT_30 0
 #define LV_FONT_MONTSERRAT_32 0
@@ -519,7 +519,7 @@
 #define LV_FONT_CUSTOM_DECLARE
 
 /*Always set a default font*/
-#define LV_FONT_DEFAULT &lv_font_montserrat_14
+#define LV_FONT_DEFAULT &lv_font_montserrat_26
 
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
@@ -583,17 +583,17 @@
 
 #define LV_WIDGETS_HAS_DEFAULT_VALUE  1
 
-#define LV_USE_ANIMIMG    1
+#define LV_USE_ANIMIMG    0
 
-#define LV_USE_ARC        1
+#define LV_USE_ARC        0
 
-#define LV_USE_BAR        1
+#define LV_USE_BAR        0
 
-#define LV_USE_BUTTON        1
+#define LV_USE_BUTTON        0
 
-#define LV_USE_BUTTONMATRIX  1
+#define LV_USE_BUTTONMATRIX  0
 
-#define LV_USE_CALENDAR   1
+#define LV_USE_CALENDAR   0
 #if LV_USE_CALENDAR
     #define LV_CALENDAR_WEEK_STARTS_MONDAY 0
     #if LV_CALENDAR_WEEK_STARTS_MONDAY
@@ -608,19 +608,19 @@
     #define LV_USE_CALENDAR_CHINESE 0
 #endif  /*LV_USE_CALENDAR*/
 
-#define LV_USE_CANVAS     1
+#define LV_USE_CANVAS     0
 
-#define LV_USE_CHART      1
+#define LV_USE_CHART      0
 
-#define LV_USE_CHECKBOX   1
+#define LV_USE_CHECKBOX   0
 
-#define LV_USE_DROPDOWN   1   /*Requires: lv_label*/
+#define LV_USE_DROPDOWN   0   /*Requires: lv_label*/
 
-#define LV_USE_IMAGE      1   /*Requires: lv_label*/
+#define LV_USE_IMAGE      0   /*Requires: lv_label*/
 
-#define LV_USE_IMAGEBUTTON     1
+#define LV_USE_IMAGEBUTTON     0
 
-#define LV_USE_KEYBOARD   1
+#define LV_USE_KEYBOARD   0
 
 #define LV_USE_LABEL      1
 #if LV_USE_LABEL
@@ -629,48 +629,48 @@
     #define LV_LABEL_WAIT_CHAR_COUNT 3  /*The count of wait chart*/
 #endif
 
-#define LV_USE_LED        1
+#define LV_USE_LED        0
 
-#define LV_USE_LINE       1
+#define LV_USE_LINE       0
 
-#define LV_USE_LIST       1
+#define LV_USE_LIST       0
 
 #define LV_USE_LOTTIE     0  /*Requires: lv_canvas, thorvg */
 
-#define LV_USE_MENU       1
+#define LV_USE_MENU       0
 
-#define LV_USE_MSGBOX     1
+#define LV_USE_MSGBOX     0
 
-#define LV_USE_ROLLER     1   /*Requires: lv_label*/
+#define LV_USE_ROLLER     0   /*Requires: lv_label*/
 
-#define LV_USE_SCALE      1
+#define LV_USE_SCALE      0
 
-#define LV_USE_SLIDER     1   /*Requires: lv_bar*/
+#define LV_USE_SLIDER     0   /*Requires: lv_bar*/
 
-#define LV_USE_SPAN       1
+#define LV_USE_SPAN       0
 #if LV_USE_SPAN
     /*A line text can contain maximum num of span descriptor */
     #define LV_SPAN_SNIPPET_STACK_SIZE 64
 #endif
 
-#define LV_USE_SPINBOX    1
+#define LV_USE_SPINBOX    0
 
-#define LV_USE_SPINNER    1
+#define LV_USE_SPINNER    0
 
-#define LV_USE_SWITCH     1
+#define LV_USE_SWITCH     0
 
-#define LV_USE_TEXTAREA   1   /*Requires: lv_label*/
+#define LV_USE_TEXTAREA   0   /*Requires: lv_label*/
 #if LV_USE_TEXTAREA != 0
     #define LV_TEXTAREA_DEF_PWD_SHOW_TIME 1500    /*ms*/
 #endif
 
-#define LV_USE_TABLE      1
+#define LV_USE_TABLE      0
 
-#define LV_USE_TABVIEW    1
+#define LV_USE_TABVIEW    0
 
-#define LV_USE_TILEVIEW   1
+#define LV_USE_TILEVIEW   0
 
-#define LV_USE_WIN        1
+#define LV_USE_WIN        0
 
 /*==================
  * THEMES
@@ -681,7 +681,7 @@
 #if LV_USE_THEME_DEFAULT
 
     /*0: Light mode; 1: Dark mode*/
-    #define LV_THEME_DEFAULT_DARK 0
+    #define LV_THEME_DEFAULT_DARK 1
 
     /*1: Enable grow on press*/
     #define LV_THEME_DEFAULT_GROW 1
@@ -694,7 +694,7 @@
 #define LV_USE_THEME_SIMPLE 1
 
 /*A theme designed for monochrome displays*/
-#define LV_USE_THEME_MONO 1
+#define LV_USE_THEME_MONO 0
 
 /*==================
  * LAYOUTS
@@ -1074,7 +1074,7 @@
 *==================*/
 
 /*Enable the examples to be built with the library*/
-#define LV_BUILD_EXAMPLES 1
+#define LV_BUILD_EXAMPLES 0
 
 /*===================
  * DEMO USAGE
--- SD2-LVGL9-Demo/src/main.cpp
+++ SD2-LVGL9-Demo/src/main.cpp
@@ -1,15 +1,23 @@
 #include <Arduino.h>
+#include <FS.h>
 #include <TFT_eSPI.h>
+#include <WiFiManager.h>
 #include <lvgl.h>
 
 #define LV_DISP_HOR_RES 240
 #define LV_DISP_VER_RES 240
+
+LV_FONT_DECLARE(led_48)
 
 int LCD_BL_PWM = 5;
 TFT_eSPI tft = TFT_eSPI();
 
 static const uint32_t buf_size = LV_DISP_HOR_RES * 20;
 static lv_color_t *dis_buf1;
+
+uint64_t ms5 = 0, ms300 = 0;
+lv_obj_t *label_date, *label_time, *label_ip;
+char buf[256] = {0};
 
 void inline lv_disp_init() {
   auto *disp = lv_display_create(LV_DISP_HOR_RES, LV_DISP_VER_RES);
@@ -38,15 +46,62 @@ void inline initDisplay() {
   analogWrite(TFT_BL, 1023 - (LCD_BL_PWM * 10));
 }
 
+void inline startConfigTime() {
+  const int timeZone = 8 * 3600;
+  configTime(timeZone, 0, "ntp6.aliyun.com", "cn.ntp.org.cn", "ntp.ntsc.ac.cn");
+  while (time(nullptr) < 8 * 3600 * 2) {
+    delay(500);
+  }
+}
+
+void inline on_300ms_tick() {
+  struct tm info;
+  getLocalTime(&info);
+  strftime(buf, 32, "%F", &info);
+  lv_label_set_text(label_date, buf);
+  strftime(buf, 32, "%T", &info);
+  lv_label_set_text(label_time, buf);
+}
+
+inline void showClientIP() {
+  lv_label_set_text(label_ip, WiFi.localIP().toString().c_str());
+}
+
 void setup() {
   Serial.begin(115200);
   lv_init();
   initDisplay();
   lv_disp_init();
+  lv_tick_set_cb([]() { return (uint32_t)millis(); });
+  WiFiManager wm;
+  tft.drawCentreString("Config WiFi", 120, 120, 4);
+  wm.autoConnect();
+  tft.drawCentreString("Config Time", 120, 120, 4);
+  startConfigTime();
+  label_date = lv_label_create(lv_scr_act());
+  lv_obj_align(label_date, LV_ALIGN_TOP_MID, 0, 8);
+  lv_label_set_text(label_date, "2024-01-01");
 
-  lv_obj_t *label = lv_label_create(lv_scr_act());
-  lv_label_set_text(label, "LVGL 9.2.2 Simple");
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+  label_time = lv_label_create(lv_scr_act());
+  lv_obj_align(label_time, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_set_style_text_font(label_time, &led_48, LV_PART_MAIN);
+  lv_label_set_text(label_time, "00:00:00");
+
+  label_ip = lv_label_create(lv_scr_act());
+  lv_obj_align(label_ip, LV_ALIGN_BOTTOM_MID, 0, -8);
+
+  showClientIP();
 }
 
-void loop() { lv_timer_handler(); }
+void loop() {
+  lv_timer_handler();
+  auto cur = millis();
+  if (cur - ms5 >= 5) {
+    ms5 = cur;
+    lv_timer_handler();
+  }
+  if (cur - ms300 >= 5) {
+    ms300 = cur;
+    on_300ms_tick();
+  }
+}
