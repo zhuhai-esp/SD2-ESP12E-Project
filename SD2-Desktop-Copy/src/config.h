177101647211dfa50c328400cce9833f27e1994c
huzongyao <huzongyao@tangbull.com>
2025-01-07 14:23:23 +0800

    fix simple demo

--- SD2-Desktop-Copy/src/config.h
+++
@@ -1,8 +0,0 @@
-#define Animate_Choice 1 // 动图选择：1,太空人图片 2,胡桃
-#define TMS 1000         // 一千毫秒
-#define WM_EN 1 // WEB配网使能标志位----WEB配网打开后会默认关闭smartconfig功能
-#define timeY 82 // 定义高度
-
-#define FONT_COLOR_HOUR 0x6D9D
-#define FONT_COLOR_MIN 0x7FC0
-#define FONT_COLOR_SEC 0xEC1D
